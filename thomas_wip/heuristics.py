0af0b905ec5e3c57eb91876206ecceaf767fd147
Zohal <zohalmir9@gmail.com>
2020-11-13 01:51:29 -0500

    sum of permutation inversions heuristic

--- thomas_wip/heuristics.py
+++ thomas_wip/heuristics.py
@@ -1,11 +1,15 @@
-def h0(puzzleArray):
+from sympy.combinatorics.partitions import Partition
+from sympy.combinatorics.permutations import Permutation
+import numpy as np
+
+def h0(puzzleArray, xDim, yDim):
     if puzzleArray[-1] == '0':
         return 0
     else:
         return 1
 
 #Hamming distance
-def hammingDistanceHeuristic(puzzleArray):
+def h1(puzzleArray, xDim, yDim):
     elementShouldEqualTo = 1
     hn = 0
     for element in puzzleArray:
@@ -23,4 +27,23 @@ def hammingDistanceHeuristic(puzzleArray):
 #         if 
 #         else:
 
+
+#sumOfPermutationInversions
+def h3(puzzleArray, xDim, yDim):
+    arrCopy = puzzleArray.copy();
+    indexOf0 = arrCopy.index('0');
+    arrCopy[indexOf0] = len(arrCopy); #change 0 to length of puzzle in order to allow Permutation.inversions()
+    arrCopy = np.hstack((0,arrCopy));
+    checkGoal1 = Permutation(arrCopy).inversions();
+    arrCopy = arrCopy[1:];
+    splitArr = np.array_split(arrCopy, yDim);
+    arrVertical = [0] * len(arrCopy);
+    count = 0;
+    for arr in splitArr:
+        for i in arr:
+            arrVertical[count] = i;
+            count = count + yDim;
+        count = 1;
+    arrVertical = np.hstack((0,arrVertical));
+    return min(checkGoal1, Permutation(arrVertical).inversions());
 
