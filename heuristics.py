2b80f4ea2826f575dae1d0b6aa1f4d62ff79a503
Hambrsoom <hampico1234@gmail.com>
2020-11-18 05:11:16 -0500

    deleted the manhattan heuristic

--- heuristics.py
+++ heuristics.py
@@ -40,39 +40,3 @@ def isDecreasing(puzzleArray, nRows, nColumns, firstSolutionList, secondSolution
             seenDecrease = seenDecrease + 1
     
     return seenDecrease
-
-# Manhattan
-def manhattan(puzzleArray, nRows, nColumns, firstSolutionList, secondSolutionList):
-
-    # Reshaping the solutions the puzzle to be like a 2d array.
-    chunksProperListFirstSolution = np.array(firstSolutionList).reshape(nColumns, nRows)
-    chunksProperListSecondSolution = np.array(secondSolutionList).reshape(nColumns, nRows)
-    chunksPuzzleList = np.array(puzzleArray).reshape(nColumns, nRows)
-    firstTotalDistance = 0
-    secondTotalDistance = 0
-
-    for y in range(len(chunksPuzzleList)):
-        for x in range(len(chunksPuzzleList[y])):
-            # Finding the proper location of each x and y according to solution one.
-            properLocationYFirstSolution, properLocationXFirstSolution = properLocationOfNode(chunksPuzzleList[y][x], chunksProperListFirstSolution)
-            
-            # Finding the proper location of each x and y according to solution two.
-            properLocationYSecondSolution, properLocationXSecondSolution = properLocationOfNode(chunksPuzzleList[y][x], chunksProperListSecondSolution)
-            
-            # Calculating the total distance of moving the elements to their proper place according to solution one & solution two
-            firstTotalDistance = firstTotalDistance + calculateDistance(properLocationXFirstSolution, properLocationYFirstSolution, y, x)
-            secondTotalDistance = secondTotalDistance + calculateDistance(properLocationXSecondSolution, properLocationYSecondSolution, y, x)
-    return min(firstTotalDistance, secondTotalDistance)
-
-# find the location of a node in a list.
-def properLocationOfNode(node, list):
-    for y in range(len(list)):
-        for x in range(len(list[y])):
-            if list[y][x] == int(node):
-                return y, x
-
-
-# Calucate the distance on both x and y access separately and then take their sum
-def calculateDistance(properX, properY, realX, realY):
-    return abs(properX-realX) + abs(properY - realY)
-
